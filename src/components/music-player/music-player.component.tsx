1d3768e71f87c333abd780f14e337a9ad31dc933
A U M Tuhin <a.tuhin@greyrook.com>
2022-06-23 00:54:23 +0200

    added audio progress volume funtionality

--- src/components/music-player/music-player.component.tsx
+++ src/components/music-player/music-player.component.tsx
@@ -1,6 +1,6 @@
-import { faBackward, faCirclePause, faForward, faPlayCircle, faVolumeHigh, faVolumeOff } from '@fortawesome/free-solid-svg-icons';
+import { faBackward, faCirclePause, faForward, faPlayCircle, faVolumeHigh, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { MutableRefObject, useEffect, useRef, useState } from 'react';
+import { ChangeEvent, MutableRefObject, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators, State } from '../../state';
@@ -11,28 +11,31 @@ const MusicPlayer = () => {
     const { isPlaying, currentSong } = useSelector((state: State) => state.music);
     const [isVolumeOff, setIsVolumeOff] = useState<boolean>(false);
     const [duration, setDuration] = useState<number>(0);
-    const [currentDuration, setCurrentDuration] = useState<String>();
+    const [currentDuration, setCurrentDuration] = useState<String>('');
+    const [currentRangeValue, setCurrentRangeVlaue] = useState<number>(0);
+    const [volume, setVolume] = useState<number>(0);
     const audioPlayer = useRef() as MutableRefObject<HTMLAudioElement>;
 
     useEffect(() => {
         if (isPlaying) {
             play();
-            setIsPlaying(true);
         } if (!isPlaying) {
             pause();
-            setIsPlaying(false);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentSong, isPlaying]);
 
     const play = async () => {
         await audioPlayer.current.play();
+        setIsPlaying(true);
         const seconds = Math.floor(audioPlayer.current.duration);
         setDuration(seconds);
+        setVolume(audioPlayer.current.volume);
     }
 
     const pause = () => {
         audioPlayer.current.pause();
+        setIsPlaying(false);
     }
 
     const togglePlay = () => {
@@ -46,6 +49,8 @@ const MusicPlayer = () => {
     }
 
     const getCurrentDuration = () => {
+        if(audioPlayer.current.currentTime === audioPlayer.current.duration ) pause();
+        setCurrentRangeVlaue(audioPlayer.current.currentTime);
         const currentTime = calculateTime(audioPlayer.current.currentTime);
         setCurrentDuration(currentTime);
     }
@@ -58,13 +63,35 @@ const MusicPlayer = () => {
         return `${returnedMinutes}:${returnedSeconds}`;
     }
 
+    const handleAudioTime = (e: ChangeEvent<HTMLInputElement>) => {
+        const currentTime = parseFloat(e.target.value);
+        audioPlayer.current.currentTime = currentTime;
+    }
+
+    const handleMute = () => {
+        setIsVolumeOff(!isVolumeOff);
+        audioPlayer.current.muted = !isVolumeOff;
+    }
+
+    const handleVolume = (e: ChangeEvent<HTMLInputElement>) => {
+        audioPlayer.current.muted = false;
+        setIsVolumeOff(false);
+        const currentVolume = parseFloat(e.target.value);
+        setVolume(currentVolume);
+        audioPlayer.current.volume = currentVolume;
+        if (currentVolume === 0) setIsVolumeOff(true);
+    }
+
     return (
         <div className="bottom-0 fixed h-20 bg-purple-500 w-screen">
             <div className='absolute w-scree'>
                 <input
                     type="range"
-                    className="absolute h-1 outline-none w-screen"
+                    className="absolute h-1 appearance-none bg-slate-700 outline-none w-screen cursor-pointer"
                     id="customRange1"
+                    value={currentRangeValue}
+                    max={duration}
+                    onChange={handleAudioTime}
                 />
             </div>
             <div className='sm:px-16 flex flex-row justify-center items-center w-full h-full gap-8'>
@@ -90,16 +117,20 @@ const MusicPlayer = () => {
                     </button>
                 </div>
                 <div className='flex justify-center items-center'>
-                    <div className='text-white text-[16px]'>{currentDuration? currentDuration : '00:00'}/{calculateTime(duration) ? calculateTime(duration): '00:00'}</div>
+                    <div className='text-white text-[16px]'>{currentDuration ? currentDuration : '00:00'}/{calculateTime(duration) ? calculateTime(duration) : '00:00'}</div>
                     <div className='flex justify-center items-center gap-1'>
-                        <button onClick={() => setIsVolumeOff(!isVolumeOff)}>
-                            {isVolumeOff && <FontAwesomeIcon className='ml-3 text-white h-5' icon={faVolumeOff} />}
+                        <button onClick={handleMute} className="w-12 h-12">
+                            {isVolumeOff && <FontAwesomeIcon className='ml-3 text-white h-5' icon={faVolumeMute} />}
                             {!isVolumeOff && <FontAwesomeIcon className='ml-3 text-white h-5' icon={faVolumeHigh} />}
                         </button>
                         <input
                             type="range"
-                            className="h-1 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none"
+                            className="h-1 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none cursor-pointer"
                             id="customRange1"
+                            step={0.1}
+                            value={volume}
+                            max={1}
+                            onChange={handleVolume}
                         />
                     </div>
                 </div>
